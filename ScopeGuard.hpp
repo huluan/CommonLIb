66a3efe9d232f1240d4ebeaf4a59ab7694095a93
TomWang10 <resonance.silent@gmail.com>
2015-06-28 17:25:04 +0800

    新增ScopeGuard类

---
+++ ScopeGuard.hpp
@@ -0,0 +1,34 @@
+#include <functional>
+#include <iostream>
+#define SCOPEGUARD_LINENAME_CAT(name, line) name##line
+#define SCOPEGUARD_LINENAME(name, line) SCOPEGUARD_LINENAME_CAT(name, line)
+
+// 因为有可能一个scope里面有好几个资源，所以我们使用EXIT加行号的方法来命名
+#define ON_SCOPE_EXIT(callback) ScopeGuard SCOPEGUARD_LINENAME(EXIT, __LINE__)(callback)
+
+class ScopeGuard
+{
+public:
+	explicit ScopeGuard(std::function<void()> onExitScope)
+		: onExitScope_(onExitScope), dismissed_(false) {}
+
+	~ScopeGuard()
+	{
+		if (!dismissed_)
+		{
+			onExitScope_();
+		}
+	}
+
+	void Dismiss()
+	{
+		dismissed_ = true;
+	}
+
+private:
+	std::function<void()> onExitScope_;
+	bool dismissed_;
+private:
+	ScopeGuard(ScopeGuard const &);
+	ScopeGuard &operator=(ScopeGuard const &);
+};
